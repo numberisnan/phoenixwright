40cba3fe1718e3afff276a73507466b19d4ec0f6
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-04-09 21:44:12 -0400

    Use canvas for image manipulation instead of gm

--- src/commands.js
+++ src/commands.js
@@ -5,24 +5,28 @@ const commands = [
         name: "objection",
         execute: function(message) {
             utils.postImage(message.channel, "https://i.kym-cdn.com/photos/images/newsfeed/000/171/527/objection-vector.png?1315394911");
+            message.delete(1000);
         }
     },
     {
         name: "takethat",
         execute: function(message) {
             utils.postImage(message.channel, "http://www.shinmh.com/blog/octubre2012/phonixwrightDS/phoenix-wright-take-that-a.png");
+            message.delete(1000);
         }
     },
     {
         name: "holdit",
         execute: function(message) {
             utils.postImage(message.channel, "https://vignette.wikia.nocookie.net/aceattorney/images/4/48/SoJ_Hold_it%21.png/revision/latest?cb=20160911003100");
+            message.delete(1000);
         }
     },
     {
         name: "oof",
         execute: function(message) {
             utils.postImage(message.channel, "https://media.giphy.com/media/IaxkRjESRHSLe/giphy.gif");
+            message.delete(1000);
         }
     },
     {
@@ -41,6 +45,7 @@ const commands = [
                     }
                 ]
             });
+            message.delete(1000);
         }
     },
     // Multiple commands command
